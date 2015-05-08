8589d27ae0c5e35db9ef8b506bb9dc3a525f006c
FedericoQuintas <federicoquintas@gmail.com>
2015-05-08 00:42:04 -0300

    PROY-3: Creo recurso básico para el User

--- src/main/java/com/proyecto/rest/resource/user/UserResource.java
+++ src/main/java/com/proyecto/rest/resource/user/UserResource.java
@@ -3,6 +3,9 @@ package com.proyecto.rest.resource.user;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.proyecto.rest.resource.user.dto.UserDTO;
 import com.proyecto.user.domain.InvertarUser;
@@ -10,10 +13,12 @@ import com.proyecto.user.service.UserService;
 
 @Controller("userResource")
 public class UserResource {
-	
+
 	@Resource
 	private UserService userService;
 
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	@ResponseBody
 	public InvertarUser store(UserDTO userDTO) {
 		return userService.store(userDTO);
 	}
