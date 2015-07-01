ade00925a7ec9f69b4d2149a0fdaa88130f88181
Alejandro Mendez <mendez.alejandrog@gmail.com>
2015-07-01 00:49:19 -0300

    Demo: Carga de assets para la demo

--- src/main/java/com/proyecto/asset/persistence/AssetDAOImpl.java
+++ src/main/java/com/proyecto/asset/persistence/AssetDAOImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.proyecto.asset.domain.Asset;
+import com.proyecto.common.currency.InvertarCurrency;
 import com.proyecto.common.exception.ObjectNotFoundException;
 
 public class AssetDAOImpl implements AssetDAO {
@@ -11,6 +12,10 @@ public class AssetDAOImpl implements AssetDAO {
 	List<Asset> assets = new ArrayList<Asset>();
 
 	Long assetsSequence = new Long(1);
+	
+	public AssetDAOImpl(){
+		loadInitialAssets();
+	}
 
 	@Override
 	public Long nextID() {
@@ -64,6 +69,42 @@ public class AssetDAOImpl implements AssetDAO {
 	public void udpate(Asset asset) {
 		remove(asset);
 		store(asset);
+	}
+	
+	private void loadInitialAssets(){
+		store(createAsset(nextID(),"Andes Energia PLC","AEN"));
+		store(createAsset(nextID(),"Agrometal S.A.I.","AGRO"));
+		store(createAsset(nextID(),"Petróleo Brasileiro S.A.","APBRA"));
+		store(createAsset(nextID(),"APSA","Alto Palermo S.A."));
+		store(createAsset(nextID(),"Autopistas del Sol S.A.","AUSO"));
+		store(createAsset(nextID(),"Banco Hipotecario S.A.","BHIP"));
+		store(createAsset(nextID(),"Boldt S.A.","BOLT"));
+		store(createAsset(nextID(),"Banco Patagonia S.A.","BPAT"));
+		store(createAsset(nextID(),"Banco Santander Río S.A.","BRIO"));
+		store(createAsset(nextID(),"Carlos Casado S.A.","CADO"));
+		store(createAsset(nextID(),"Caputo S.A.","CAPU"));
+		store(createAsset(nextID(),"Capex S.A.","CAPX"));
+		store(createAsset(nextID(),"Carboclor S.A.","CARC"));
+		store(createAsset(nextID(),"Endesa Costanera S.A.","CECO2"));
+		store(createAsset(nextID(),"Celulosa Argentina S.A.","CELU"));
+		store(createAsset(nextID(),"Central Puerto S.A.","CEPU2"));
+		store(createAsset(nextID(),"Camuzzi Gas Pampeana S.A.","CGPA2"));
+		store(createAsset(nextID(),"Colorin S.A.","COLO"));
+		store(createAsset(nextID(),"Continental Urbana S.A.I.","COUR"));
+		store(createAsset(nextID(),"Cresud S.A.","CRES"));
+		store(createAsset(nextID(),"Consultatio S.A.","CTIO"));
+		store(createAsset(nextID(),"Domec S.A.","DOME"));
+		store(createAsset(nextID(),"Dycasa S.A.","DYCA"));
+		store(createAsset(nextID(),"Bodegas Esmeralda S.A.","ESME"));
+		store(createAsset(nextID(),"Angel Estrada y Cia. S.A.","ESTR"));
+		store(createAsset(nextID(),"Ferrum S.A.","FERR"));
+		store(createAsset(nextID(),"Fiplasto S.A.","FIPL"));
+		store(createAsset(nextID(),"Garovaglio y Zorraquin S.A.","GARO"));
+	}
+	
+	private Asset createAsset(Long id, String description, String ticker){
+		InvertarCurrency currency = InvertarCurrency.ARS;
+		return new Asset(id, description, ticker, currency);
 	}
 
 }
