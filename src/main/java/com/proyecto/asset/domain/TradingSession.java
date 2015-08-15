5555c6b9e03dffcf7e9c471938ace7f0e43e436f
ivanoliveri <ipoliveri@gmail.com>
2015-08-15 00:40:14 -0300

    development: Agrego campos para medias moviles e indicadores técnicos

--- src/main/java/com/proyecto/asset/domain/TradingSession.java
+++ src/main/java/com/proyecto/asset/domain/TradingSession.java
@@ -10,6 +10,26 @@ public class TradingSession {
 	private Double minPrice;
 	private Date tradingDate;
 	private Integer volume;
+	
+	private Double sma_7;
+	private Double sma_21;
+	private Double sma_50;
+	private Double sma_200;
+	
+	private Double ema_7;
+	private Double ema_21;
+	private Double ema_50;
+	private Double ema_200;
+	
+	private Double momentum_7;
+	private Double momentum_21;
+	private Double momentum_50;
+	private Double momentum_200;
+	
+	private Double rsi_7;
+	private Double rsi_21;
+	private Double rsi_50;
+	private Double rsi_200;	
 
 	public TradingSession() {
 	}
@@ -61,4 +81,134 @@ public class TradingSession {
 	public void setVolume(Integer volume) {
 		this.volume = volume;
 	}
+
+	public Double getSma_7() {
+		return sma_7;
+	}
+
+	public void setSma_7(Double sma_7) {
+		this.sma_7 = sma_7;
+	}
+
+	public Double getSma_21() {
+		return sma_21;
+	}
+
+	public void setSma_21(Double sma_21) {
+		this.sma_21 = sma_21;
+	}
+
+	public Double getSma_50() {
+		return sma_50;
+	}
+
+	public void setSma_50(Double sma_50) {
+		this.sma_50 = sma_50;
+	}
+
+	public Double getSma_200() {
+		return sma_200;
+	}
+
+	public void setSma_200(Double sma_200) {
+		this.sma_200 = sma_200;
+	}
+
+	public Double getEma_7() {
+		return ema_7;
+	}
+
+	public void setEma_7(Double ema_7) {
+		this.ema_7 = ema_7;
+	}
+
+	public Double getEma_21() {
+		return ema_21;
+	}
+
+	public void setEma_21(Double ema_21) {
+		this.ema_21 = ema_21;
+	}
+
+	public Double getEma_50() {
+		return ema_50;
+	}
+
+	public void setEma_50(Double ema_50) {
+		this.ema_50 = ema_50;
+	}
+
+	public Double getEma_200() {
+		return ema_200;
+	}
+
+	public void setEma_200(Double ema_200) {
+		this.ema_200 = ema_200;
+	}
+
+	public Double getMomentum_7() {
+		return momentum_7;
+	}
+
+	public void setMomentum_7(Double momentum_7) {
+		this.momentum_7 = momentum_7;
+	}
+
+	public Double getMomentum_21() {
+		return momentum_21;
+	}
+
+	public void setMomentum_21(Double momentum_21) {
+		this.momentum_21 = momentum_21;
+	}
+
+	public Double getMomentum_50() {
+		return momentum_50;
+	}
+
+	public void setMomentum_50(Double momentum_50) {
+		this.momentum_50 = momentum_50;
+	}
+
+	public Double getMomentum_200() {
+		return momentum_200;
+	}
+
+	public void setMomentum_200(Double momentum_200) {
+		this.momentum_200 = momentum_200;
+	}
+
+	public Double getRsi_7() {
+		return rsi_7;
+	}
+
+	public void setRsi_7(Double rsi_7) {
+		this.rsi_7 = rsi_7;
+	}
+
+	public Double getRsi_21() {
+		return rsi_21;
+	}
+
+	public void setRsi_21(Double rsi_21) {
+		this.rsi_21 = rsi_21;
+	}
+
+	public Double getRsi_50() {
+		return rsi_50;
+	}
+
+	public void setRsi_50(Double rsi_50) {
+		this.rsi_50 = rsi_50;
+	}
+
+	public Double getRsi_200() {
+		return rsi_200;
+	}
+
+	public void setRsi_200(Double rsi_200) {
+		this.rsi_200 = rsi_200;
+	}
+	
+	
 }
--- src/main/java/com/proyecto/asset/domain/factory/TradingSessionFactory.java
+++ src/main/java/com/proyecto/asset/domain/factory/TradingSessionFactory.java
@@ -20,6 +20,26 @@ public class TradingSessionFactory {
 		tradingSession.setOpeningPrice(tradingSessionDTO.getOpeningPrice());
 		tradingSession.setVolume(tradingSessionDTO.getVolume());
 		
+		tradingSession.setSma_7(tradingSessionDTO.getSma_7());
+		tradingSession.setSma_21(tradingSessionDTO.getSma_21());
+		tradingSession.setSma_50(tradingSessionDTO.getSma_50());
+		tradingSession.setSma_200(tradingSessionDTO.getSma_200());		
+		
+		tradingSession.setEma_7(tradingSessionDTO.getEma_7());
+		tradingSession.setEma_21(tradingSessionDTO.getEma_21());
+		tradingSession.setEma_50(tradingSessionDTO.getEma_50());
+		tradingSession.setEma_200(tradingSessionDTO.getEma_200());	
+		
+		tradingSession.setRsi_7(tradingSessionDTO.getRsi_7());
+		tradingSession.setRsi_21(tradingSessionDTO.getRsi_21());
+		tradingSession.setRsi_50(tradingSessionDTO.getRsi_50());
+		tradingSession.setRsi_200(tradingSessionDTO.getRsi_200());
+		
+		tradingSession.setMomentum_7(tradingSessionDTO.getMomentum_7());
+		tradingSession.setMomentum_21(tradingSessionDTO.getMomentum_21());
+		tradingSession.setMomentum_50(tradingSessionDTO.getMomentum_50());
+		tradingSession.setMomentum_200(tradingSessionDTO.getMomentum_200());		
+		
 		return tradingSession;
 
 	}
