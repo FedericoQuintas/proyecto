a1a5345af5114e5a19fc8930aa2029da54be0495
ivanoliveri <ipoliveri@gmail.com>
2015-11-14 16:54:52 -0300

    development: Sigo sacando bonos falopa y arreglo mas gaps

--- src/main/resources/load-currencies.py
+++ src/main/resources/load-currencies.py
@@ -87,33 +87,39 @@ for oneLink in links:
         index = 0
         for span in row.findAll("span"):
             if index % 2 == 1:
-                newTradingSession.exchangeDate = str(row)[111:121]
-                newTradingSession.sellPrice = float(span.string[1:])
-                newCurrency.tradingSessions.append(newTradingSession)
-                if str(newTradingSession.exchangeDate) in ["13/06/2014","30/03/2015","27/10/2015"]:
-                    if str(newTradingSession.exchangeDate)== "13/06/2014":
-                        specialTradingSession = InvertarExchangeSession()
-                        specialTradingSession.buyPrice = newTradingSession.buyPrice
-                        specialTradingSession.sellPrice = newTradingSession.sellPrice
-                        specialTradingSession.exchangeDate = "16/06/2014"
-                        newCurrency.tradingSessions.append(specialTradingSession)
-                    if str(newTradingSession.exchangeDate)== "30/03/2015":
-                        specialTradingSession = InvertarExchangeSession()
-                        specialTradingSession.buyPrice = newTradingSession.buyPrice
-                        specialTradingSession.sellPrice = newTradingSession.sellPrice
-                        specialTradingSession.exchangeDate = "31/03/2015"
-                        newCurrency.tradingSessions.append(specialTradingSession)
-                    if str(newTradingSession.exchangeDate)== "27/10/2015":
-                        specialTradingSession = InvertarExchangeSession()
-                        specialTradingSession.buyPrice = newTradingSession.buyPrice
-                        specialTradingSession.sellPrice = newTradingSession.sellPrice
-                        specialTradingSession.exchangeDate = "28/10/2015"
-                        newCurrency.tradingSessions.append(specialTradingSession)
-                        specialTradingSession2 = InvertarExchangeSession()
-                        specialTradingSession2.buyPrice = newTradingSession.buyPrice
-                        specialTradingSession2.sellPrice = newTradingSession.sellPrice
-                        specialTradingSession2.exchangeDate = "29/10/2015"
-                        newCurrency.tradingSessions.append(specialTradingSession2)
+                if str(row)[111:121] not in ["24/12/2014","06/11/2014"]:
+                    newTradingSession.exchangeDate = str(row)[111:121]
+                    newTradingSession.sellPrice = float(span.string[1:])
+                    newCurrency.tradingSessions.append(newTradingSession)
+                    if str(newTradingSession.exchangeDate) in ["27/11/2013","13/06/2014","30/03/2015","27/10/2015"]:
+                        if str(newTradingSession.exchangeDate)== "27/11/2013":
+                            specialTradingSession = InvertarExchangeSession()
+                            specialTradingSession.buyPrice = newTradingSession.buyPrice
+                            specialTradingSession.sellPrice = newTradingSession.sellPrice
+                            specialTradingSession.exchangeDate = "28/11/2013"
+                        if str(newTradingSession.exchangeDate)== "13/06/2014":
+                            specialTradingSession = InvertarExchangeSession()
+                            specialTradingSession.buyPrice = newTradingSession.buyPrice
+                            specialTradingSession.sellPrice = newTradingSession.sellPrice
+                            specialTradingSession.exchangeDate = "16/06/2014"
+                            newCurrency.tradingSessions.append(specialTradingSession)
+                        if str(newTradingSession.exchangeDate)== "30/03/2015":
+                            specialTradingSession = InvertarExchangeSession()
+                            specialTradingSession.buyPrice = newTradingSession.buyPrice
+                            specialTradingSession.sellPrice = newTradingSession.sellPrice
+                            specialTradingSession.exchangeDate = "31/03/2015"
+                            newCurrency.tradingSessions.append(specialTradingSession)
+                        if str(newTradingSession.exchangeDate)== "27/10/2015":
+                            specialTradingSession = InvertarExchangeSession()
+                            specialTradingSession.buyPrice = newTradingSession.buyPrice
+                            specialTradingSession.sellPrice = newTradingSession.sellPrice
+                            specialTradingSession.exchangeDate = "28/10/2015"
+                            newCurrency.tradingSessions.append(specialTradingSession)
+                            specialTradingSession2 = InvertarExchangeSession()
+                            specialTradingSession2.buyPrice = newTradingSession.buyPrice
+                            specialTradingSession2.sellPrice = newTradingSession.sellPrice
+                            specialTradingSession2.exchangeDate = "29/10/2015"
+                            newCurrency.tradingSessions.append(specialTradingSession2)
             else:
                 newTradingSession = InvertarExchangeSession()
                 newTradingSession.buyPrice = float(span.string[1:])
