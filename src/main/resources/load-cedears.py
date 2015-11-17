decb3b409c8fcaba2c2de1080ffdc4c23cae554d
ivanoliveri <ipoliveri@gmail.com>
2015-11-17 00:22:46 -0300

    development: Agrego cálculo de Indicadores Técnicos para CEDEAR

--- src/main/resources/load-cedears.py
+++ src/main/resources/load-cedears.py
@@ -1,10 +1,11 @@
-from yahoo_finance import Share
 from datetime import *
-import json
-import urllib.request
 import io
-import requests
 import csv
+import json
+from datetime import datetime
+import time
+import requests
+import urllib
 
 class InvertarCedear:
 
@@ -89,9 +90,10 @@ csvsToDownload.append(Link("CEDEARAIG","American Internat. Group Inc. (CEDEAR)",
 csvsToDownload.append(Link("CEDEARC","Citigroup (CEDEAR)","http://www.ravaonline.com/v2/empresas/precioshistoricos.php?e=CEDEARC&csv=1"))
 
 finalCedears = []
+cedears = []
 
 for oneLink in csvsToDownload:
-    print("Procesando:",oneLink.ticker)
+    print("Descargando:",oneLink.ticker)
     webpage = urllib.request.urlopen(oneLink.url)
     datareader = csv.reader(io.TextIOWrapper(webpage))
     currentCedear = InvertarCedear(oneLink.ticker,oneLink.name)
@@ -100,26 +102,204 @@ for oneLink in csvsToDownload:
     for row in datareader:
         if row[4] != "cierre":
             currentCedear.tradingSessions.append(InvertarTradingSession(row[4],row[1],row[2],row[3],row[0],row[5]))
-    finalCedears.append(currentCedear)
+    cedears.append(currentCedear)
+
+for oneCedear in cedears:
+
+    print("Procesando:",oneCedear.ticker)
+    myInvertarCedear = InvertarCedear(oneCedear.ticker,oneCedear.name)
+    currentCedear = oneCedear
+    myInvertarCedear.description = oneCedear.description
+    myInvertarCedear.currency = "ARS"
+    myInvertarCedear.tradingSessions = []
+
+    index = 0
+
+    bonds=[]
+    for oneTradingSession in oneCedear.tradingSessions:
+        if str(oneTradingSession.tradingDate)!="2015-10-12":
+            currentTradingSession = InvertarTradingSession(oneTradingSession.closingPrice, oneTradingSession.openingPrice,
+                                                           oneTradingSession.maxPrice,oneTradingSession.minPrice,
+                                                           oneTradingSession.tradingDate,oneTradingSession.volume)
+
+            currentTradingSession.sma_7= 0.0
+            currentTradingSession.sma_21= 0.0
+            currentTradingSession.sma_50= 0.0
+            currentTradingSession.sma_200= 0.0
+
+            currentTradingSession.ema_7= 0.0
+            currentTradingSession.ema_21= 0.0
+            currentTradingSession.ema_50= 0.0
+            currentTradingSession.ema_200= 0.0
+
+            currentTradingSession.momentum_7= 0.0
+            currentTradingSession.momentum_21= 0.0
+            currentTradingSession.momentum_50= 0.0
+            currentTradingSession.momentum_200= 0.0
+
+            currentTradingSession.rsi_7 = 0.0
+            currentTradingSession.rsi_21 = 0.0
+            currentTradingSession.rsi_50 = 0.0
+            currentTradingSession.rsi_200 = 0.0
+
+            currentTradingSession.macd_macd_line = 0.0
+            currentTradingSession.macd_signal_line = 0.0
+            currentTradingSession.macd_histogram = 0.0
+
+            periods = [7,12,21,26,50,200]
+
+            for period in periods:
+                if len(myInvertarCedear.tradingSessions) >= period-1:
+
+                    min = len(myInvertarCedear.tradingSessions) - (period-1)
+                    max = len(myInvertarCedear.tradingSessions)
+
+                    cumulativeCloses = 0.0
+
+                    upward_movements = 0.0
+                    downward_movements = 0.0
+
+                    last_closing_price = 0.0
+
+                    for index in range(min,max):
+                        if float(myInvertarCedear.tradingSessions[index].closingPrice)>=float(myInvertarCedear.tradingSessions[index-1].closingPrice):
+                            upward_movements += (float(myInvertarCedear.tradingSessions[index].closingPrice)-float(myInvertarCedear.tradingSessions[index-1].closingPrice))
+                        else:
+                            downward_movements += (float(myInvertarCedear.tradingSessions[index-1].closingPrice)-float(myInvertarCedear.tradingSessions[index].closingPrice))
+                        last_closing_price = myInvertarCedear.tradingSessions[index].closingPrice
+                        cumulativeCloses += float(myInvertarCedear.tradingSessions[index].closingPrice)
+
+                    if float(currentTradingSession.closingPrice)>=float(last_closing_price):
+                        upward_movements += (float(currentTradingSession.closingPrice)-float(last_closing_price))
+                    else:
+                        downward_movements += (float(last_closing_price)-float(currentTradingSession.closingPrice))
+
+                    current_sma = (cumulativeCloses + float(currentTradingSession.closingPrice)) / period
+                    current_momentum = round(float(currentTradingSession.closingPrice) - float(myInvertarCedear.tradingSessions[min].closingPrice),2)
+
+                    if downward_movements==0.0:
+                        current_rsi=100.0
+                    else:
+                        current_rsi = 100 - (100 / ((upward_movements/period)/(downward_movements/period)+1))
+
+                    current_ema = 0.0
+
+                    if period==7:
+                        currentTradingSession.sma_7 = round(current_sma,2)
+                        currentTradingSession.momentum_7 = round(current_momentum,2)
+                        currentTradingSession.rsi_7 = round(current_rsi,2)
+                        if min== 0:
+                            current_ema = current_sma
+                        else:
+                            current_ema = last_ema_7 * (1-(float(2) / float(period+1))) + (
+                                                        float(currentTradingSession.closingPrice)
+                                                        * (float(2)/float(period+1)))
+                        currentTradingSession.ema_7 = round(current_ema,2)
+                        last_ema_7 = current_ema
+                    elif period==21:
+                        currentTradingSession.sma_21 = round(current_sma,2)
+                        currentTradingSession.momentum_21 = round(current_momentum,2)
+                        currentTradingSession.rsi_21 = round(current_rsi,2)
+                        if min== 0:
+                            current_ema = current_sma
+                        else:
+                            current_ema = last_ema_21 * (1-(float(2) / float(period+1))) + (
+                                                        float(currentTradingSession.closingPrice)
+                                                        * (float(2)/float(period+1)))
+                        currentTradingSession.ema_21 = round(current_ema,2)
+                        last_ema_21 = current_ema
+                    elif period==50:
+                        currentTradingSession.sma_50 = round(current_sma,2)
+                        currentTradingSession.momentum_50 = round(current_momentum,2)
+                        currentTradingSession.rsi_50 = round(current_rsi,2)
+                        if min== 0:
+                            current_ema = current_sma
+                        else:
+                            current_ema = last_ema_50 * (1-(float(2) / float(period+1))) + (
+                                                        float(currentTradingSession.closingPrice)
+                                                        * (float(2)/float(period+1)))
+                        currentTradingSession.ema_50 = round(current_ema,2)
+                        last_ema_50 = current_ema
+                    elif period==200:
+                        currentTradingSession.sma_200 = round(current_sma,2)
+                        currentTradingSession.momentum_200 = round(current_momentum,2)
+                        currentTradingSession.rsi_200 = round(current_rsi,2)
+                        if min== 0:
+                            current_ema = current_sma
+                        else:
+                            current_ema = last_ema_200 * (1-(float(2) / float(period+1))) + (
+                                                        float(currentTradingSession.closingPrice)
+                                                        * (float(2)/float(period+1)))
+                        currentTradingSession.ema_200 = round(current_ema,2)
+                        last_ema_200 = current_ema
+                    elif period==12:
+                        if min== 0:
+                            current_ema = current_sma
+                        else:
+                            current_ema = last_ema_12 * (1-(float(2) / float(period+1))) + (
+                                                        float(currentTradingSession.closingPrice)
+                                                        * (float(2)/float(period+1)))
+                        last_ema_12 = current_ema
+                        currentTradingSession.macd_macd_line = current_ema
+                    elif period==26:
+                        if min== 0:
+                            current_ema = current_sma
+                        else:
+                            current_ema = last_ema_26 * (1-(float(2) / float(period+1))) + (
+                                                        float(currentTradingSession.closingPrice)
+                                                        * (float(2)/float(period+1)))
+                        last_ema_26 = current_ema
+                        currentTradingSession.macd_macd_line -= current_ema
+
+            # Only inserts last 3 years but we take in consideration for the calculations the last 5 years
+            if datetime.strptime(currentTradingSession.tradingDate,"%Y-%m-%d").date()>=date.today() - timedelta(days=1085):
+                myInvertarCedear.tradingSessions.append(currentTradingSession)
+
+    finalCedears.append(myInvertarCedear)
 
 # Open connection to API
 conn = requests.request('POST', url=login_url, headers=headers, data=json.dumps(credentials))
 session_cookie = conn.cookies
 
-for aCedear in finalCedears:
-    json_ts = []
-    aCedear.lastTradingPrice = aCedear.tradingSessions[len(aCedear.tradingSessions) -1].closingPrice
-    for ts in aCedear.tradingSessions:
-        if str(ts.tradingDate) != "2015-10-12":
-            ts.tradingDate = int(datetime.strptime(ts.tradingDate, "%Y-%m-%d").timestamp()) * 1000
-            json_ts.append(ts.__dict__)
-    aCedear.tradingSessions = json_ts
+for oneInvertarCedear in finalCedears:
+    macd_macd_line = []
+    last_ema_9 = 0.0
+    index = 0
+    for oneInvertarTradingSession in oneInvertarCedear.tradingSessions:
+        oneInvertarTradingSession.tradingDate = int(time.mktime(datetime.strptime(oneInvertarTradingSession.tradingDate,"%Y-%m-%d").timetuple())) * 1000
+        oneInvertarTradingSession.adjClosingPrice = 0.0
+        index = index +1
+        if index == len(oneInvertarCedear.tradingSessions)-1:
+            oneInvertarCedear.lastTradingPrice = oneInvertarTradingSession.closingPrice
+        macd_macd_line.append(oneInvertarTradingSession.macd_macd_line)
+        if len(macd_macd_line) >= 9:
+            min = len(macd_macd_line) - 9
+            max = len(macd_macd_line)
+            sum_macd_macd_line=0.0
+            for index in range(min,max):
+                sum_macd_macd_line += macd_macd_line[index]
+            current_sma = sum_macd_macd_line/float(9)
+            current_ema = current_sma
+            if min != 0:
+                current_ema = last_ema_9 * (1-(float(2) / float(10))) + (
+                                            float(oneInvertarTradingSession.macd_macd_line)
+                                            * (float(2)/float(10)))
+            last_ema_9 = current_ema
+            oneInvertarTradingSession.macd_signal_line = current_ema
 
-    r =requests.request('POST', url=store_url, headers=headers, cookies=session_cookie, data=json.dumps(aCedear.__dict__))
+    #oneInvertarCedear.tradingSessions = map(lambda x: x.__dict__, oneInvertarCedear.tradingSessions)
+
+    # La linea de arriba deberia andar pero no lo hace, por eso la horripilancia de abajo
+    json_ts = []
+    for ts in oneInvertarCedear.tradingSessions:
+        json_ts.append(ts.__dict__)
+    oneInvertarCedear.tradingSessions = json_ts
+
+    r =requests.request('POST', url=store_url, headers=headers, cookies=session_cookie, data=json.dumps(oneInvertarCedear.__dict__))
 
     if r.status_code == 200:
-        print("Cargado:", aCedear.ticker)
+        print("Cargado:",oneInvertarCedear.ticker)
     else:
-        print("Error en la carga del CEDEAR {}: {}".format(aCedear.ticker, r.content))
+        print("Error en la carga del cedear {}: {}".format(oneInvertarCedear.ticker, r.content))
 
-
+print("Fin de Carga de Cedears")
